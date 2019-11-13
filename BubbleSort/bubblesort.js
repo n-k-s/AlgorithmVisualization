4afaf9de27d29faceab148907a6393b07d6d43b0
n-k-s <49578275+n-k-s@users.noreply.github.com>
2019-11-13 13:26:24 -0600

    initialize array and comments

--- BubbleSort/bubblesort.js
+++ BubbleSort/bubblesort.js
@@ -1,6 +1,12 @@
+//initialize the array
+let bubbleArray = new Array(10);
 function setup() {
     createCanvas(windowWidth, windowHeight);
-
+    //create random values in the array
+    for (let i = 0; i < bubbleArray.length; i++)
+    {
+        bubbleArray[i] = floor(random(30));
+    }
 }
 
 function draw() {
